52be8bf1f124d95f50b52f6e308f67f11a081824
Balthazar Potet <balthazarpotet@gmail.com>
2017-12-30 13:22:18 +0100

    Added serialization for the training set
    Former-commit-id: 0a245e76b6dfa71c2acd332dd91551949d9aba48

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@ use std::io;
 use std::io::prelude::*;
 use std::fs::File;
 
+
+extern crate serde;
+extern crate serde_json;
+extern crate bincode;
+extern crate rmp_serde;
+
 extern crate nn;
 use nn::Network;
 
@@ -9,30 +15,31 @@ extern crate nalgebra;
 use nalgebra as na;
 use na::DVector;
 
-const NB_TRAIN_SAMPLES: usize = 60_000;
+const NB_TRAIN_SAMPLES: usize = 50_000;
+const NB_TEST_SAMPLES: usize = 10_000;
 const SKIP_BYTES_TRAIN_IMAGES: usize = 16;
 const SKIP_BYTES_TRAIN_LABELS: usize = 8;
 const IMAGE_SIZE: usize = 784;
 const LABEL_SIZE: usize = 1;
 
 
-fn open_mnist_train() -> Vec<(DVector<u8>, DVector<u8>)> {
+fn open_mnist_train() -> Vec<(DVector<f64>, DVector<f64>)> {
 
-	let mut res: Vec<(DVector<u8>, DVector<u8>)> = Vec::new();
+	let mut res: Vec<(DVector<f64>, DVector<f64>)> = Vec::new();
 
 	let mut train_images = File::open("resources/train-images.idx3-ubyte").unwrap();
 	let mut train_labels = File::open("resources/train-labels.idx1-ubyte").unwrap();
 
-	train_images.read(&mut [0; SKIP_BYTES_TRAIN_IMAGES]);
-	train_labels.read(&mut [0; SKIP_BYTES_TRAIN_LABELS]);
+	train_images.read(&mut [0; SKIP_BYTES_TRAIN_IMAGES]).unwrap();
+	train_labels.read(&mut [0; SKIP_BYTES_TRAIN_LABELS]).unwrap();
 
 	let mut image = [0; IMAGE_SIZE];
 	let mut label = [0; LABEL_SIZE];
 	for i in 0..NB_TRAIN_SAMPLES {
-		train_images.read(&mut image);
-		train_labels.read(&mut label);
-		res.push((DVector::from_row_slice(IMAGE_SIZE, &image[..]),
-					DVector::from_row_slice(LABEL_SIZE, &label[..])));
+		train_images.read(&mut image).unwrap();
+		train_labels.read(&mut label).unwrap();
+		res.push((DVector::from_row_slice(IMAGE_SIZE, &image.iter().map(|x| *x as f64).collect::<Vec<_>>()[..]),
+					DVector::from_row_slice(LABEL_SIZE, &label.iter().map(|x| *x as f64).collect::<Vec<_>>()[..])));
 
 		println!("{:?}", i);
 	}
@@ -41,5 +48,9 @@ fn open_mnist_train() -> Vec<(DVector<u8>, DVector<u8>)> {
 }
 
 fn main() {
-    open_mnist_train();
+
+	let data: Vec<(DVector<f64>, DVector<f64>)> = 
+				bincode::deserialize_from(&mut File::open("resources/train_mnist.bincode").unwrap(),
+					bincode::Infinite).unwrap();
+
 }
